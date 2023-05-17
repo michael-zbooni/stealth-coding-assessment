59100712edb94efd9d7afb163a031fb49e64b109
Richard Michael Coo <richard.michael.coo@gmail.com>
2023-05-18 00:34:02 +0800

    :card_file_box: db: switch Postgre naming to snake_case

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,12 +2,22 @@ import 'reflect-metadata'
 
 import { DataSource, DataSourceOptions } from 'typeorm'
 import { postgresConfig } from './constants'
-import { OAuthUser } from './entities/oauth-user.entity'
-import { OAuthClient } from './entities/oauth-client.entity'
-import { OAuthScope } from './entities/oauth-scope.entity'
-import { OAuthToken } from './entities/oauth-token.entity'
-import { OAuthCode } from './entities/oauth-code.entity'
 import { SeederOptions } from 'typeorm-extension'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
+
+function createSeederGlob() {
+  const { SEEDER_NAMES = '' } = process.env
+  const multipleSeeds = SEEDER_NAMES?.includes(',')
+
+  // it doesn't treat {oauth-scope}.seeder.ts as a glob, so curlies only for multiple seeders
+  if (multipleSeeds) {
+    return `{${SEEDER_NAMES}}`
+  }
+
+  return SEEDER_NAMES ? `${SEEDER_NAMES}` : '*'
+}
+
+console.log('foo', `src/seeds/${createSeederGlob()}.seeder.ts`)
 
 export const mainDataSource = new DataSource({
   ...postgresConfig,
@@ -15,6 +25,7 @@ export const mainDataSource = new DataSource({
   logging: true,
   entities: ['src/entities/**/*.entity.ts'],
   migrations: ['src/migrations/**/*.ts'],
+  namingStrategy: new SnakeNamingStrategy(),
   subscribers: [],
-  seeds: ['src/seeds/**/*.seeder.ts'],
+  seeds: [`src/seeds/${createSeederGlob()}.seeder.ts`],
 } as DataSourceOptions & SeederOptions)
