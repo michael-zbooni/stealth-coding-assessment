5f480d1f25c1e703d1be007b2a4129c1f62a51b2
Richard Michael Coo <richard.michael.coo@gmail.com>
2023-05-19 11:26:31 +0800

    :white_check_mark: tests: add tests for UserService.getUser[s]

--- src/services/user.service.test.ts
+++ src/services/user.service.test.ts
@@ -4,9 +4,11 @@ import { EntityManager, EntityNotFoundError, Repository } from 'typeorm'
 import bcrypt from 'bcrypt'
 import crypto from 'crypto'
 
-import { BCRYPT_ROUNDS } from '../constants'
+import { BCRYPT_ROUNDS, defaultPaginationLimits } from '../constants'
+import { pick } from 'lodash/fp'
 
 type MockValue = never
+const { USERS: USERS_DEFAULT_PAGINATION_LIMIT } = defaultPaginationLimits
 
 // TODO: put all mock classes in one place
 class MockUserRepository extends Repository<OAuthUser> {
@@ -14,6 +16,7 @@ class MockUserRepository extends Repository<OAuthUser> {
     super(OAuthUser, {} as EntityManager)
   }
 
+  find = jest.fn()
   findOneOrFail = jest.fn()
   save = jest.fn()
 }
@@ -239,6 +242,183 @@ describe('UserService', () => {
         hashedPassword: 'some-new-bcrypt-hash',
         activationToken: 'i-am-a-very-sensitive-token',
         active: true,
+        createdAt: new Date('2023-01-01'),
+        updatedAt: new Date('2023-01-02'),
+      })
+    })
+  })
+
+  describe('#getUsers', () => {
+    const service = new UserService(baseRepositoryMock)
+    const user = Object.assign(new OAuthUser(), {
+      id: 1,
+      email: 'mike@gmail.com',
+      firstName: 'Mike',
+      lastName: 'Coo',
+      createdAt: new Date('2023-01-01'),
+      updatedAt: new Date('2023-01-02'),
+    } as OAuthUser)
+    const anotherUser = Object.assign(new OAuthUser(), {
+      id: 2,
+      email: 'richard@gmail.com',
+      firstName: 'Richard',
+      lastName: 'Yap',
+      createdAt: new Date('2023-01-01'),
+      updatedAt: new Date('2023-01-02'),
+    } as OAuthUser)
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it('returns a list of users with complete info (minus sensitive info) if the request is authenticated', async () => {
+      baseRepositoryMock.find.mockResolvedValue([user, anotherUser])
+
+      const actual = await service.getUsers({ authenticated: true, limit: 5, offset: 15 })
+      expect(baseRepositoryMock.find).toHaveBeenCalledWith({
+        where: {
+          active: true,
+        },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          createdAt: true,
+          updatedAt: true,
+          hashedPassword: false,
+          activationToken: false,
+        },
+        take: 5,
+        skip: 15,
+      })
+
+      // there are undefined values like hashedPassword, so toStrictEqual fails
+      // https://jestjs.io/docs/expect#toequalvalue
+      expect(actual).toEqual([
+        {
+          id: 1,
+          email: 'mike@gmail.com',
+          firstName: 'Mike',
+          lastName: 'Coo',
+          createdAt: new Date('2023-01-01'),
+          updatedAt: new Date('2023-01-02'),
+        },
+
+        {
+          id: 2,
+          email: 'richard@gmail.com',
+          firstName: 'Richard',
+          lastName: 'Yap',
+          createdAt: new Date('2023-01-01'),
+          updatedAt: new Date('2023-01-02'),
+        },
+      ])
+    })
+
+    it('returns a list of users with first name only (minus sensitive info) if the request is unauthenticated', async () => {
+      baseRepositoryMock.find.mockResolvedValue([user, anotherUser].map(pick(['firstName'])))
+
+      const actual = await service.getUsers({ authenticated: false })
+
+      expect(baseRepositoryMock.find).toHaveBeenCalledWith({
+        where: {
+          active: true,
+        },
+        select: {
+          id: false,
+          email: false,
+          firstName: true,
+          lastName: false,
+          createdAt: false,
+          updatedAt: false,
+          hashedPassword: false,
+          activationToken: false,
+        },
+        take: USERS_DEFAULT_PAGINATION_LIMIT,
+        skip: 0,
+      })
+      expect(actual).toStrictEqual([{ firstName: 'Mike' }, { firstName: 'Richard' }])
+    })
+  })
+
+  describe('#getUser', () => {
+    const service = new UserService(baseRepositoryMock)
+    const user = Object.assign(new OAuthUser(), {
+      id: 1,
+      email: 'mike@gmail.com',
+      firstName: 'Mike',
+      lastName: 'Coo',
+      createdAt: new Date('2023-01-01'),
+      updatedAt: new Date('2023-01-02'),
+    } as OAuthUser)
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it('returns the user with complete info (minus sensitive info) if the request is authenticated', async () => {
+      baseRepositoryMock.findOneOrFail.mockResolvedValue(user)
+
+      const actual = await service.getUser({ userId: 1, authenticated: true })
+
+      expect(baseRepositoryMock.findOneOrFail).toHaveBeenCalledWith({
+        where: {
+          id: 1,
+          active: true,
+        },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          createdAt: true,
+          updatedAt: true,
+          hashedPassword: false,
+          activationToken: false,
+        },
+      })
+
+      // there are undefined values like hashedPassword, so toStrictEqual fails
+      // https://jestjs.io/docs/expect#toequalvalue
+      expect(actual).toEqual({
+        id: 1,
+        email: 'mike@gmail.com',
+        firstName: 'Mike',
+        lastName: 'Coo',
+        createdAt: new Date('2023-01-01'),
+        updatedAt: new Date('2023-01-02'),
+      })
+    })
+
+    it('returns the first name only if the request is unauthenticated', async () => {
+      baseRepositoryMock.findOneOrFail.mockResolvedValue(user)
+
+      const actual = await service.getUser({ userId: 1, authenticated: false })
+      expect(baseRepositoryMock.findOneOrFail).toHaveBeenCalledWith({
+        where: {
+          id: 1,
+          active: true,
+        },
+        select: {
+          id: false,
+          email: false,
+          firstName: true,
+          lastName: false,
+          createdAt: false,
+          updatedAt: false,
+          hashedPassword: false,
+          activationToken: false,
+        },
+      })
+
+      // there are undefined values like hashedPassword, so toStrictEqual fails
+      // https://jestjs.io/docs/expect#toequalvalue
+      expect(actual).toEqual({
+        id: 1,
+        email: 'mike@gmail.com',
+        firstName: 'Mike',
+        lastName: 'Coo',
         createdAt: new Date('2023-01-01'),
         updatedAt: new Date('2023-01-02'),
       })
