c2b93f21aca6bb9d088e6d50927a0d85495c51dd
Richard Michael Coo <richard.michael.coo@gmail.com>
2023-05-22 11:09:47 +0800

    :white_check_mark: tests: factor out mockResponse (same for all test cases)
    not only that, it also does not get mutated

--- src/middlewares/validate-pagination-params.test.ts
+++ src/middlewares/validate-pagination-params.test.ts
@@ -3,14 +3,19 @@ import { validatePaginationParams } from './validate-pagination-params'
 import { HttpStatusCode } from '../enums/http-status-code.enum'
 
 describe('validatePaginationParams', () => {
+  const mockResponse = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as unknown as Express.Response
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('returns a 400 if the limit is not a number', () => {
     const mockRequest = {
       query: { limit: 'not-a-number' },
     } as unknown as Express.Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Express.Response
     const mockNext = jest.fn()
 
     validatePaginationParams(mockRequest, mockResponse, mockNext)
@@ -24,10 +29,6 @@ describe('validatePaginationParams', () => {
     const mockRequest = {
       query: { limit: '0' },
     } as unknown as Express.Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Express.Response
     const mockNext = jest.fn()
 
     validatePaginationParams(mockRequest, mockResponse, mockNext)
@@ -41,10 +42,6 @@ describe('validatePaginationParams', () => {
     const mockRequest = {
       query: { limit: '101' },
     } as unknown as Express.Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Express.Response
     const mockNext = jest.fn()
 
     validatePaginationParams(mockRequest, mockResponse, mockNext)
@@ -58,10 +55,6 @@ describe('validatePaginationParams', () => {
     const mockRequest = {
       query: { offset: 'not-a-number' },
     } as unknown as Express.Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Express.Response
     const mockNext = jest.fn()
 
     validatePaginationParams(mockRequest, mockResponse, mockNext)
@@ -75,10 +68,6 @@ describe('validatePaginationParams', () => {
     const mockRequest = {
       query: { offset: '-1' },
     } as unknown as Express.Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Express.Response
     const mockNext = jest.fn()
 
     validatePaginationParams(mockRequest, mockResponse, mockNext)
@@ -92,10 +81,6 @@ describe('validatePaginationParams', () => {
     const mockRequest = {
       query: { limit: '1', offset: '0' },
     } as unknown as Express.Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Express.Response
     const mockNext = jest.fn()
 
     validatePaginationParams(mockRequest, mockResponse, mockNext)
@@ -109,10 +94,6 @@ describe('validatePaginationParams', () => {
     const mockRequest = {
       query: { offset: '0' },
     } as unknown as Express.Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Express.Response
     const mockNext = jest.fn()
 
     validatePaginationParams(mockRequest, mockResponse, mockNext)
@@ -126,10 +107,6 @@ describe('validatePaginationParams', () => {
     const mockRequest = {
       query: { limit: '1' },
     } as unknown as Express.Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Express.Response
     const mockNext = jest.fn()
 
     validatePaginationParams(mockRequest, mockResponse, mockNext)
@@ -143,10 +120,6 @@ describe('validatePaginationParams', () => {
     const mockRequest = {
       query: {},
     } as unknown as Express.Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Express.Response
     const mockNext = jest.fn()
 
     validatePaginationParams(mockRequest, mockResponse, mockNext)
